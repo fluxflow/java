6a6a0d09089598c7f1def2984fe61cb8aae52c3b
Andre <Everythingiszeelo@gmail.com>
2017-09-21 17:17:50 -0400

    test
    test

---
+++ DynamicCar.java
@@ -0,0 +1,10 @@
+package assignment1bits;
+
+public interface DynamicCar {
+
+	  public int createRandomNumber(int min, int max);
+	  public void start(int h, int l);
+	  public void StartRunning();
+	  public void run(int h, int l);
+		it works lol
+}
